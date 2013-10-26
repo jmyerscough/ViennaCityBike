670a295712c4243fade5a488d8ce1a9776dc1f01
Jason Myerscough <jmyerscough1@googlemail.com>
2013-10-26 15:47:32 +0200

    added a util that checks if the device has a network connection.

---
+++ src/com/myerscough/viennacitybike/utils/ViennaCityBikeUtils.java
@@ -0,0 +1,32 @@
+package com.myerscough.viennacitybike.utils;
+
+import android.app.Activity;
+import android.content.Context;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
+
+public class ViennaCityBikeUtils 
+{
+	/**
+	 * Iterates all network infos and returns true if one of them is available for
+	 * sending data.
+	 * @param activity
+	 * @return true if a network connection exists. Otherwise, flase is returned.
+	 * @author Jason Myerscough
+	 */
+	public static boolean isDeviceOnline(Activity activity)
+	{
+		ConnectivityManager cm = (ConnectivityManager)activity.getSystemService(Context.CONNECTIVITY_SERVICE);
+		NetworkInfo [] netInfo = cm.getAllNetworkInfo();
+		
+		if (netInfo != null)
+		{
+			for (int i=0; i < netInfo.length; i++)
+			{
+				if (netInfo[i].isConnected())
+					return true;
+			}
+		}
+		return true;
+	}
+}
